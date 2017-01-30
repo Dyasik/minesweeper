f37cad945fe391046f6cc614e41cccfeba23ec8c
Denis Seletkov <rysds2@gmail.com>
2017-01-30 22:37:51 +0400

    Refactored bombs' placement probability & indents.

--- script.js
+++ script.js
@@ -14,328 +14,328 @@ var TILE_CLASS = 'tile';
 document.addEventListener("DOMContentLoaded", start);
 
 function start(event) {
-	var FIELD = document.querySelector("#field");
-	var BTN = document.querySelector('#re');
+    var FIELD = document.querySelector("#field");
+    var BTN = document.querySelector('#re');
 
-	var SMILE = document.querySelector('#smile');
-	var DEAD = document.querySelector('#dead');
-	var BOSS = document.querySelector('#boss');
-  var FLAG = document.querySelector("#flag");
-  var BOMB = document.querySelector("#bomb");
+    var SMILE = document.querySelector('#smile');
+    var DEAD = document.querySelector('#dead');
+    var BOSS = document.querySelector('#boss');
+    var FLAG = document.querySelector("#flag");
+    var BOMB = document.querySelector("#bomb");
 
-	var tiles = [];
-	// returns the SVG node with requested icon
-	function getSVG(name) {
-		var obj;
-		switch (name) {
-			case 'flag':
-				obj = FLAG.cloneNode(true);
-				break;
-			case 'bomb':
-				obj = BOMB.cloneNode(true);
-				break;
-			case 'nobomb':
-				obj = BOMB.cloneNode(true);
-				var tmp = document.createElementNS(xmlns, "path");
-				tmp.setAttributeNS(null, "d", "M 3 3 L 13 13 \
-									   M 3 13 L 13 3");
-				tmp.setAttributeNS(null, "stroke", "red");
-				tmp.setAttributeNS(null, "stroke-width", "2");
-				obj.appendChild(tmp);
-				break;
-			case 'smile':
-				obj = SMILE.cloneNode(true);
-				break;
-			case 'dead':
-				obj = DEAD.cloneNode(true);
-				break;
-			case 'boss':
-				obj = BOSS.cloneNode(true);
-				break;
-			default:
-				return;
-		}
-		obj.style.display = 'inline';
-		return obj;
-	}
+    var tiles = [];
+    // returns the SVG node with requested icon
+    function getSVG(name) {
+        var obj;
+        switch (name) {
+            case 'flag':
+                obj = FLAG.cloneNode(true);
+                break;
+            case 'bomb':
+                obj = BOMB.cloneNode(true);
+                break;
+            case 'nobomb':
+            obj = BOMB.cloneNode(true);
+                var tmp = document.createElementNS(xmlns, "path");
+                tmp.setAttributeNS(null, "d", "M 3 3 L 13 13 \
+                                               M 3 13 L 13 3");
+                tmp.setAttributeNS(null, "stroke", "red");
+                tmp.setAttributeNS(null, "stroke-width", "2");
+                obj.appendChild(tmp);
+                break;
+            case 'smile':
+                obj = SMILE.cloneNode(true);
+                break;
+            case 'dead':
+                obj = DEAD.cloneNode(true);
+                break;
+            case 'boss':
+                obj = BOSS.cloneNode(true);
+                break;
+            default:
+                return;
+        }
+        obj.style.display = 'inline';
+        return obj;
+    }
 
-	var playing = false;
+    var playing = false;
 
-	BTN.addEventListener('click', function () {
-		if (playing && !confirm('Do you really want to restart?')) {
-			return;
-		}
+    BTN.addEventListener('click', function () {
+        if (playing && !confirm('Do you really want to restart?')) {
+            return;
+        }
 
-		FIELD.innerHTML = '';
-		playing = true;
+        FIELD.innerHTML = '';
+        playing = true;
 
-		BTN.removeChild(BTN.lastChild);
-		BTN.appendChild(getSVG('smile'));
+        BTN.removeChild(BTN.lastChild);
+        BTN.appendChild(getSVG('smile'));
 
-		configs = [];
-		REVEALED_COUNT = 0;
+        configs = [];
+        REVEALED_COUNT = 0;
 
-		// filling in the bombs
-		var bomb_count = 0;
-		var i = 0;
-		while (true) {
-			if (bomb_count == BOMBS_COUNT) {
-				while (i < TILES_COUNT) {
-					configs[i % TILES_COUNT] = {
-						isBomb: false,
-						isOpen: false,
-						isMarked: false, 
-						i: i % TILES_COUNT
-					};
-					i++;
-				} 
-				break;
-			}
-			if (Math.round(Math.random() * 100) < 19) {
-				if (configs[i % TILES_COUNT] == undefined || configs[i % TILES_COUNT].isBomb == false) {
-					configs[i % TILES_COUNT] = {
-						isBomb: true,
-						isOpen: false,
-						isMarked: false,
-						i: i % TILES_COUNT
-					};
-					bomb_count++;
-				}
-			} else {
-				if (configs[i % TILES_COUNT] == undefined) {
-					configs[i % TILES_COUNT] = {
-						isBomb: false,
-						isOpen: false,
-						isMarked: false, 
-						i: i % TILES_COUNT
-					};
-				}
-			}
-			i++;
-		}
+        // filling in the bombs
+        var bomb_count = 0;
+        var i = 0;
+        while (true) {
+            if (bomb_count == BOMBS_COUNT) {
+                while (i < TILES_COUNT) {
+                    configs[i % TILES_COUNT] = {
+                        isBomb: false,
+                        isOpen: false,
+                        isMarked: false, 
+                        i: i % TILES_COUNT
+                    };
+                    i++;
+                } 
+                break;
+            }
+            if (Math.random() + 0.01 < BOMBS_COUNT / TILES_COUNT) {
+                if (!configs[i % TILES_COUNT] || !configs[i % TILES_COUNT].isBomb) {
+                    configs[i % TILES_COUNT] = {
+                        isBomb: true,
+                        isOpen: false,
+                        isMarked: false,
+                        i: i % TILES_COUNT
+                    };
+                    bomb_count++;
+                }
+            } else {
+                if (configs[i % TILES_COUNT] == undefined) {
+                    configs[i % TILES_COUNT] = {
+                        isBomb: false,
+                        isOpen: false,
+                        isMarked: false, 
+                        i: i % TILES_COUNT
+                    };
+                }
+            }
+            i++;
+        }
 
-		// calculating the digits
-		// __ __ __
-		//|0_|1_|2_|
-		//|3_|ij|4_|
-		//|5_|6_|7_|
-		//
-		function dig_calc (i, j) {
-			return (configs[i * COLS + j].isBomb) ? 1 : 0;
-		}
-		for (i = 0; i < TILES_COUNT; i++) {
-			if (!configs[i].isBomb) {
-				var result = 0;
-				var row = Math.floor(i / COLS);
-				var col = i % COLS;
-				var tmp_row;
-				var tmp_col;
-				var tmp_i;
-				// 0
-				tmp_row = row - 1;
-				tmp_col = col - 1;
-				if ((tmp_row != -1)&&(tmp_col != -1)) result += dig_calc(tmp_row, tmp_col);
-				// 1
-				tmp_row = row - 1;
-				tmp_col = col;
-				if (tmp_row != -1) result += dig_calc(tmp_row, tmp_col);
-				// 2
-				tmp_row = row - 1;
-				tmp_col = col + 1;
-				if ((tmp_row != -1)&&(tmp_col != COLS)) result += dig_calc(tmp_row, tmp_col);
-				// 3
-				tmp_row = row;
-				tmp_col = col - 1;
-				if (tmp_col != -1) result += dig_calc(tmp_row, tmp_col);
-				// 4
-				tmp_row = row;
-				tmp_col = col + 1;
-				if (tmp_col != COLS) result += dig_calc(tmp_row, tmp_col);
-				// 5
-				tmp_row = row + 1;
-				tmp_col = col - 1;
-				if ((tmp_row != ROWS)&&(tmp_col != -1)) result += dig_calc(tmp_row, tmp_col);
-				// 6
-				tmp_row = row + 1;
-				tmp_col = col;
-				if (tmp_row != ROWS) result += dig_calc(tmp_row, tmp_col);
-				// 7
-				tmp_row = row + 1;
-				tmp_col = col + 1;
-				if ((tmp_row != ROWS)&&(tmp_col != COLS)) result += dig_calc(tmp_row, tmp_col);
+        // calculating the digits
+        // __ __ __
+        //|0_|1_|2_|
+        //|3_|ij|4_|
+        //|5_|6_|7_|
+        //
+        function dig_calc (i, j) {
+            return (configs[i * COLS + j].isBomb) ? 1 : 0;
+        }
+        for (i = 0; i < TILES_COUNT; i++) {
+            if (!configs[i].isBomb) {
+                var result = 0;
+                var row = Math.floor(i / COLS);
+                var col = i % COLS;
+                var tmp_row;
+                var tmp_col;
+                var tmp_i;
+                // 0
+                tmp_row = row - 1;
+                tmp_col = col - 1;
+                if ((tmp_row != -1)&&(tmp_col != -1)) result += dig_calc(tmp_row, tmp_col);
+                // 1
+                tmp_row = row - 1;
+                tmp_col = col;
+                if (tmp_row != -1) result += dig_calc(tmp_row, tmp_col);
+                // 2
+                tmp_row = row - 1;
+                tmp_col = col + 1;
+                if ((tmp_row != -1)&&(tmp_col != COLS)) result += dig_calc(tmp_row, tmp_col);
+                // 3
+                tmp_row = row;
+                tmp_col = col - 1;
+                if (tmp_col != -1) result += dig_calc(tmp_row, tmp_col);
+                // 4
+                tmp_row = row;
+                tmp_col = col + 1;
+                if (tmp_col != COLS) result += dig_calc(tmp_row, tmp_col);
+                // 5
+                tmp_row = row + 1;
+                tmp_col = col - 1;
+                if ((tmp_row != ROWS)&&(tmp_col != -1)) result += dig_calc(tmp_row, tmp_col);
+                // 6
+                tmp_row = row + 1;
+                tmp_col = col;
+                if (tmp_row != ROWS) result += dig_calc(tmp_row, tmp_col);
+                // 7
+                tmp_row = row + 1;
+                tmp_col = col + 1;
+                if ((tmp_row != ROWS)&&(tmp_col != COLS)) result += dig_calc(tmp_row, tmp_col);
 
-				configs[i].digit = result;
-			}
-		}
+                configs[i].digit = result;
+            }
+        }
 
-		tiles = [];
-		for (i = 0; i < TILES_COUNT; i++){
-			tiles[i] = new Tile(configs[i]);
-			tiles[i].append();
-		}
-	});
+        tiles = [];
+        for (i = 0; i < TILES_COUNT; i++){
+            tiles[i] = new Tile(configs[i]);
+            tiles[i].append();
+        }
+    });
 
-	BTN.click();
+    BTN.click();
 
-	// when clicking on an empty (digit == 0) tile
-	function reveal(i) {
-		var row = Math.floor(i / COLS);
-		var col = i % COLS;
-		var tmp_row;
-		var tmp_col;
-		var tmp_i;
-		// 0
-		tmp_row = row - 1;
-		tmp_col = col - 1;
-		tmp_i = tmp_row * COLS + tmp_col;
-		if (tmp_row != -1 && tmp_col != -1 && tiles[tmp_i].freeToReveal()) {
-			tiles[tmp_i].click();
-		}
-		// 1
-		tmp_row = row - 1;
-		tmp_col = col;
-		tmp_i = tmp_row * COLS + tmp_col;
-		if (tmp_row != -1 && tiles[tmp_i].freeToReveal()) {
-			tiles[tmp_i].click();
-		}
-		// 2
-		tmp_row = row - 1;
-		tmp_col = col + 1;
-		tmp_i = tmp_row * COLS + tmp_col;
-		if (tmp_row != -1 && tmp_col != COLS && tiles[tmp_i].freeToReveal()) {
-			tiles[tmp_i].click();
-		}
-		// 3
-		tmp_row = row;
-		tmp_col = col - 1;
-		tmp_i = tmp_row * COLS + tmp_col;
-		if (tmp_col != -1 && tiles[tmp_i].freeToReveal()) {
-			tiles[tmp_i].click();
-		}
-		// 4
-		tmp_row = row;
-		tmp_col = col + 1;
-		tmp_i = tmp_row * COLS + tmp_col;
-		if (tmp_col != COLS && tiles[tmp_i].freeToReveal()) {
-			tiles[tmp_i].click();
-		}
-		// 5
-		tmp_row = row + 1;
-		tmp_col = col - 1;
-		tmp_i = tmp_row * COLS + tmp_col;
-		if (tmp_row != ROWS && tmp_col != -1 && tiles[tmp_i].freeToReveal()) {
-			tiles[tmp_i].click();
-		}
-		// 6
-		tmp_row = row + 1;
-		tmp_col = col;
-		tmp_i = tmp_row * COLS + tmp_col;
-		if (tmp_row != ROWS && tiles[tmp_i].freeToReveal()) {
-			tiles[tmp_i].click();
-		}
-		// 7
-		tmp_row = row + 1;
-		tmp_col = col + 1;
-		tmp_i = tmp_row * COLS + tmp_col;
-		if (tmp_row != ROWS && tmp_col != COLS && tiles[tmp_i].freeToReveal()) {
-			tiles[tmp_i].click();
-		}
-	}
+    // when clicking on an empty (digit == 0) tile
+    function reveal(i) {
+        var row = Math.floor(i / COLS);
+        var col = i % COLS;
+        var tmp_row;
+        var tmp_col;
+        var tmp_i;
+        // 0
+        tmp_row = row - 1;
+        tmp_col = col - 1;
+        tmp_i = tmp_row * COLS + tmp_col;
+        if (tmp_row != -1 && tmp_col != -1 && tiles[tmp_i].freeToReveal()) {
+            tiles[tmp_i].click();
+        }
+        // 1
+        tmp_row = row - 1;
+        tmp_col = col;
+        tmp_i = tmp_row * COLS + tmp_col;
+        if (tmp_row != -1 && tiles[tmp_i].freeToReveal()) {
+            tiles[tmp_i].click();
+        }
+        // 2
+        tmp_row = row - 1;
+        tmp_col = col + 1;
+        tmp_i = tmp_row * COLS + tmp_col;
+        if (tmp_row != -1 && tmp_col != COLS && tiles[tmp_i].freeToReveal()) {
+            tiles[tmp_i].click();
+        }
+        // 3
+        tmp_row = row;
+        tmp_col = col - 1;
+        tmp_i = tmp_row * COLS + tmp_col;
+        if (tmp_col != -1 && tiles[tmp_i].freeToReveal()) {
+            tiles[tmp_i].click();
+        }
+        // 4
+        tmp_row = row;
+        tmp_col = col + 1;
+        tmp_i = tmp_row * COLS + tmp_col;
+        if (tmp_col != COLS && tiles[tmp_i].freeToReveal()) {
+            tiles[tmp_i].click();
+        }
+        // 5
+        tmp_row = row + 1;
+        tmp_col = col - 1;
+        tmp_i = tmp_row * COLS + tmp_col;
+        if (tmp_row != ROWS && tmp_col != -1 && tiles[tmp_i].freeToReveal()) {
+            tiles[tmp_i].click();
+        }
+        // 6
+        tmp_row = row + 1;
+        tmp_col = col;
+        tmp_i = tmp_row * COLS + tmp_col;
+        if (tmp_row != ROWS && tiles[tmp_i].freeToReveal()) {
+            tiles[tmp_i].click();
+        }
+        // 7
+        tmp_row = row + 1;
+        tmp_col = col + 1;
+        tmp_i = tmp_row * COLS + tmp_col;
+        if (tmp_row != ROWS && tmp_col != COLS && tiles[tmp_i].freeToReveal()) {
+            tiles[tmp_i].click();
+        }
+    }
 
-	// Tile class
-	function Tile(config) {
-    	// instance to be returned
-		var $ = {
-			isBomb: config.isBomb,
-			isOpen: config.isOpen,
-			isMarked: config.isMarked,
-			digit: config.digit
-		};
+    // Tile class
+    function Tile(config) {
+        // instance to be returned
+        var $ = {
+            isBomb: config.isBomb,
+            isOpen: config.isOpen,
+            isMarked: config.isMarked,
+            digit: config.digit
+        };
 
-	  	var tile = document.createElement("div");
-		tile.setAttribute("class", TILE_CLASS);
+        var tile = document.createElement("div");
+        tile.setAttribute("class", TILE_CLASS);
 
-		var I = config.i;
+        var I = config.i;
 
-		tile.addEventListener("click", function (event) {
-			if (!$.isOpen && !$.isMarked && playing) {
-				if ($.isBomb) {
-					tile.classList.add('boom');
-					tiles.forEach( function(e) {
-						e.onLose();
-					});
-					tile.appendChild(getSVG('bomb'));
-					playing = false;
-					BTN.removeChild(BTN.lastChild);
-					BTN.appendChild(getSVG('dead'));
-					alert('BOOOM! Game over :(\nPress on that sad face to play again.');
-				} else {
-					tile.classList.add('open');
-          			$.isOpen = true;
-					tile.classList.add('d' + $.digit);
-					tile.innerHTML = $.digit ? $.digit : '';
-					if (!$.digit) {
-						reveal(I);
-					}
-					REVEALED_COUNT++;
-					if (TILES_COUNT - BOMBS_COUNT === REVEALED_COUNT) {
-						playing = false;
-						tiles.forEach( function(e) {
-							e.onWin();
-						});
-						BTN.removeChild(BTN.lastChild);
-						BTN.appendChild(getSVG('boss'));
-						alert('You win!\nPress on that smiley face to play again.');
-					}
-				}
-			} 
-		});
+        tile.addEventListener("click", function (event) {
+            if (!$.isOpen && !$.isMarked && playing) {
+                if ($.isBomb) {
+                    tile.classList.add('boom');
+                    tiles.forEach( function(e) {
+                        e.onLose();
+                    });
+                    tile.appendChild(getSVG('bomb'));
+                    playing = false;
+                    BTN.removeChild(BTN.lastChild);
+                    BTN.appendChild(getSVG('dead'));
+                    alert('BOOOM! Game over :(\nPress on that sad face to play again.');
+                } else {
+                    tile.classList.add('open');
+                    $.isOpen = true;
+                    tile.classList.add('d' + $.digit);
+                    tile.innerHTML = $.digit ? $.digit : '';
+                    if (!$.digit) {
+                        reveal(I);
+                    }
+                    REVEALED_COUNT++;
+                    if (TILES_COUNT - BOMBS_COUNT === REVEALED_COUNT) {
+                        playing = false;
+                        tiles.forEach( function(e) {
+                            e.onWin();
+                        });
+                        BTN.removeChild(BTN.lastChild);
+                        BTN.appendChild(getSVG('boss'));
+                        alert('You win!\nPress on that smiley face to play again.');
+                    }
+                }
+            } 
+        });
 
-		tile.addEventListener("contextmenu", function (event) {
-			event.preventDefault();
-			if (!$.isOpen && playing) {
-				if ($.isMarked) {
-          			$.isMarked = false;
-					tile.removeChild(tile.lastElementChild);
-				} else {
-          			$.isMarked = true;
-					tile.appendChild(getSVG('flag'));
-				}
-			}
-		});
+        tile.addEventListener("contextmenu", function (event) {
+            event.preventDefault();
+            if (!$.isOpen && playing) {
+                if ($.isMarked) {
+                    $.isMarked = false;
+                    tile.removeChild(tile.lastElementChild);
+                } else {
+                    $.isMarked = true;
+                    tile.appendChild(getSVG('flag'));
+                }
+            }
+        });
 
-	    $.onLose = function () {
-			if ($.isMarked && !$.isBomb) {
-				tile.classList.add('open');
-				tile.removeChild(tile.lastElementChild);
-				tile.appendChild(getSVG('nobomb'));
-				return;
-			}
-			if ($.isBomb && !$.isMarked) {
-				tile.classList.add('open');
-				tile.appendChild(getSVG('bomb'));
-			}
-		};
+        $.onLose = function () {
+            if ($.isMarked && !$.isBomb) {
+                tile.classList.add('open');
+                tile.removeChild(tile.lastElementChild);
+                tile.appendChild(getSVG('nobomb'));
+                return;
+            }
+            if ($.isBomb && !$.isMarked) {
+                tile.classList.add('open');
+                tile.appendChild(getSVG('bomb'));
+            }
+        };
 
-		$.onWin = function () {
-			if ($.isBomb && !$.isMarked) {
-				tile.classList.add('open');
-				tile.appendChild(getSVG('bomb'));
-			}
-		};
+        $.onWin = function () {
+            if ($.isBomb && !$.isMarked) {
+                tile.classList.add('open');
+                tile.appendChild(getSVG('bomb'));
+            }
+        };
 
-		$.append = function () {
-			FIELD.appendChild(tile);
-		};
+        $.append = function () {
+            FIELD.appendChild(tile);
+        };
 
-	    $.click = function () {
-	      tile.click();
-	    };
+        $.click = function () {
+            tile.click();
+        };
 
-	    $.freeToReveal = function () {
-			return !($.isOpen || $.isBomb || $.isMarked);
-		};
+        $.freeToReveal = function () {
+            return !($.isOpen || $.isBomb || $.isMarked);
+        };
 
-		return $;
-	}
+        return $;
+    }
 }
